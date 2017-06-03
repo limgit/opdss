e3cf2079a5153dd3dbc66053020f6765d29178bb
YeongJun Cho <skywave@live.co.kr>
2017-06-04 00:30:38 +0900

    Change WebServer to contain FlaskServer class

--- src/webserver/web_server.py
+++ src/webserver/web_server.py
@@ -8,16 +8,6 @@ from controller.manager import ObjectManager, TemplateManager, SignageManager
 
 class WebServer:
     def __init__(self, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
-        self._flask_server = FlaskServer(obj_mng, tpl_mng, sgn_mng)
-        self._flask_io_server = FlaskIOServer(sgn_mng)
-
-    def start(self):
-        self._flask_server.start()
-        self._flask_io_server.start()
-
-
-class FlaskServer(threading.Thread):
-    def __init__(self, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
         self._app = flask.Flask(__name__)
         self._app.static_folder = str(tpl_mng._dir_root.resolve())
         self._app.add_url_rule('/favicon.ico', 'favicon', lambda: '')
@@ -29,10 +19,16 @@ class FlaskServer(threading.Thread):
         self._tpl_mng = tpl_mng
         self._sgn_mng = sgn_mng
 
+        self._socket_io = flask_socketio.SocketIO(self._app)
+        self._io_server = FlaskIOServer()
+
+        self._socket_io.on_namespace(self._io_server)
+
         super().__init__()
 
-    def run(self):
-        self._app.run()
+    def start(self):
+        threading.Thread(target=self._app.run).start()
+        threading.Thread(target=self._socket_io.run, args=(self._app, None, 5100)).start()
 
     def handle_signage_list(self) -> str:
         return ' '.join(['<a href="/{0}">{0}</a>'.format(str(x)) for x in self._sgn_mng.signages.keys()])
@@ -44,16 +40,9 @@ class FlaskServer(threading.Thread):
         return self._app.send_static_file(path)
 
 
-class FlaskIOServer(threading.Thread):
-    def __init__(self, sgn_mng: SignageManager):
-        self._sgn_mng = sgn_mng
-        self._app = flask.Flask(__name__)
-        self._socket_io = flask_socketio.SocketIO(self._app)
-
-        super().__init__()
-
-    def run(self):
-        self._socket_io.run(self._app, port=5100)
+class FlaskIOServer(flask_socketio.Namespace):
+    def on_connect(self):
+        return True
 
 
 class Client:
