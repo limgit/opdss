f26ea82472611d43382dabf2dd6af6f61c4f1a09
YeongJun Cho <skywave@live.co.kr>
2017-06-03 23:34:40 +0900

    Change WebServer to consist of FlaskServer class and FlaskIOServer class

--- src/webserver/web_server.py
+++ src/webserver/web_server.py
@@ -1,9 +1,22 @@
+import threading
+
 import flask
+import flask_socketio
 
 from controller.manager import ObjectManager, TemplateManager, SignageManager
 
 
 class WebServer:
+    def __init__(self, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
+        self._flask_server = FlaskServer(obj_mng, tpl_mng, sgn_mng)
+        self._flask_io_server = FlaskIOServer(sgn_mng)
+
+    def start(self):
+        self._flask_server.start()
+        self._flask_io_server.start()
+
+
+class FlaskServer(threading.Thread):
     def __init__(self, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
         self._app = flask.Flask(__name__)
         self._app.static_folder = str(tpl_mng._dir_root.resolve())
@@ -16,7 +29,9 @@ class WebServer:
         self._tpl_mng = tpl_mng
         self._sgn_mng = sgn_mng
 
-    def start(self):
+        super().__init__()
+
+    def run(self):
         self._app.run()
 
     def handle_signage_list(self) -> str:
@@ -27,6 +42,18 @@ class WebServer:
 
     def handle_template_static(self, path: str) -> str:
         return self._app.send_static_file(path)
+
+
+class FlaskIOServer(threading.Thread):
+    def __init__(self, sgn_mng: SignageManager):
+        self._sgn_mng = sgn_mng
+        self._app = flask.Flask(__name__)
+        self._socket_io = flask_socketio.SocketIO(self._app)
+
+        super().__init__()
+
+    def run(self):
+        self._socket_io.run(self._app, port=5100)
 
 
 class Client:
