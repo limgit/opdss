c54e27793f1aa5ae2def44af681c3af148252c5b
YeongJun Cho <skywave@live.co.kr>
2017-06-05 20:20:43 +0900

    Implement clients to connect to a specific channel, not a specific signage

--- src/webserver/web_server.py
+++ src/webserver/web_server.py
@@ -3,18 +3,19 @@ import threading
 import flask
 import flask_socketio
 
-from controller.manager import ObjectManager, TemplateManager, SignageManager
+from controller.manager import ObjectManager, TemplateManager, SignageManager, ChannelManager
 
 
 class WebServer:
-    def __init__(self, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
+    def __init__(self, chn_mng: ChannelManager, obj_mng: ObjectManager, tpl_mng: TemplateManager, sgn_mng: SignageManager):
         self._app = flask.Flask(__name__)
         self._app.static_folder = str(tpl_mng._dir_root.resolve())
         self._app.add_url_rule('/favicon.ico', 'favicon', lambda: '')
-        self._app.add_url_rule('/', 'handle_signage_list', self.handle_signage_list)
-        self._app.add_url_rule('/<signage_id>', 'handle_signage', self.handle_signage)
+        self._app.add_url_rule('/', 'handle_channel_list', self.handle_channel_list)
+        self._app.add_url_rule('/<channel_id>', 'handle_channel', self.handle_channel)
         self._app.add_url_rule('/_/<path:path>', 'handle_template_static', self.handle_template_static)
 
+        self._chn_mng = chn_mng
         self._obj_mng = obj_mng
         self._tpl_mng = tpl_mng
         self._sgn_mng = sgn_mng
@@ -30,11 +31,11 @@ class WebServer:
         threading.Thread(target=self._app.run).start()
         threading.Thread(target=self._socket_io.run, args=(self._app, None, 5100)).start()
 
-    def handle_signage_list(self) -> str:
-        return ' '.join(['<a href="/{0}">{0}</a>'.format(str(x)) for x in self._sgn_mng.signages.keys()])
+    def handle_channel_list(self) -> str:
+        return ' '.join(['<a href="/{0}">{0}</a>'.format(str(x)) for x in self._chn_mng.channels.keys()])
 
-    def handle_signage(self, signage_id: str) -> str:
-        return self._sgn_mng.get_signage(signage_id).render()
+    def handle_channel(self, channel_id: str) -> str:
+        return self._chn_mng.get_channel(channel_id).signage.render()
 
     def handle_template_static(self, path: str) -> str:
         return self._app.send_static_file(path)
