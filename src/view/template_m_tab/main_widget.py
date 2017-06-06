54a6a14384e696b54c0a228ac6ab0cd1498a3e21
HS Lim <bookman1209@gmail.com>
2017-06-06 22:42:37 +0900

    Make template tab to display frame template list

--- src/view/template_m_tab/main_widget.py
+++ src/view/template_m_tab/main_widget.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import (QWidget, QTreeWidget, QTreeWidgetItem,
                              QHBoxLayout)
 
+import utils.utils as Utils
 from .template_widget import TemplateWidget
 from controller.manager import ObjectManager, TemplateManager, SignageManager
 from view.resource_manager import ResourceManager
@@ -19,11 +20,18 @@ class TemplateManagementTab(QWidget):
         self.init_ui()
 
     def template_to_tree_item(self):
-        # TODO: This is dummy code. Add functionality
         frame_label_item = QTreeWidgetItem([self._res['frameLabel']])
+        for frame_tpl_id in self._tpl_mng.frame_templates.keys():
+            frame_tpl = self._tpl_mng.get_frame_template(frame_tpl_id)
+            frame_text = Utils.gen_ui_text(frame_tpl.definition.name, frame_tpl.id)
+            frame_item = QTreeWidgetItem([frame_text])
+            frame_label_item.addChild(frame_item)
+
         scene_label_item = QTreeWidgetItem([self._res['sceneLabel']])
         for scene_tpl_id in self._tpl_mng.scene_templates.keys():
-            scene_item = QTreeWidgetItem([scene_tpl_id])
+            scene_tpl = self._tpl_mng.get_scene_template(scene_tpl_id)
+            scene_text = Utils.gen_ui_text(scene_tpl.definition.name, scene_tpl.id)
+            scene_item = QTreeWidgetItem([scene_text])
             scene_label_item.addChild(scene_item)
         return [frame_label_item, scene_label_item]
 
@@ -52,5 +60,6 @@ class TemplateManagementTab(QWidget):
                 pass
             else:
                 # Selected one is frame/scene
-                self._template_widget.load_data_on_ui(item.parent().text(0), self._tpl_mng, item_text)
+                tpl_id = Utils.ui_text_to_id(item_text)
+                self._template_widget.load_data_on_ui(item.parent().text(0), self._tpl_mng, tpl_id)
                 # item.parent().text(0) is self._res['frameLabel'] or self._res['sceneLabel']
