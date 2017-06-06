2e752372783665f8883b30055118bb4579743415
HS Lim <bookman1209@gmail.com>
2017-06-06 13:45:19 +0900

    Rename variable to do not hide builtins

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -10,8 +10,8 @@ def validate_id(id_value: Optional[str]) -> None:
 
 
 # Utils for view
-def gen_ui_text(name: str, id: str) -> str:
-    return name + " [" + id + "]"
+def gen_ui_text(name: str, id_value: str) -> str:
+    return name + " [" + id_value + "]"
 
 
 def ui_text_to_id(text: str) -> str:
