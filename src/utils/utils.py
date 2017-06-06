73d835ed7efb31f52e4c0bc36ff5d4c60fda9ded
HS Lim <bookman1209@gmail.com>
2017-06-06 14:16:43 +0900

    Fix bug in regex of id validation

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -5,7 +5,7 @@ import re
 
 
 def validate_id(id_value: Optional[str]) -> None:
-    if id_value is not None and not re.match('[a-z]+[a-z0-9_]*', id_value):
+    if id_value is not None and not re.match('^[a-z]+[a-z0-9_]*$', id_value):
         raise AttributeError()
 
 
